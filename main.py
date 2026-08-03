36ab35e699968a2e5b5f5d7900119a6136be1c3f
MiguelPacheco7 <miguelpacheco.br@gmail.com>
2026-08-03 16:18:49 -0300

    Login security

--- main.py
+++ main.py
@@ -116,15 +116,23 @@ def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
             headers={"WWW-Authenticate": "Basic"},
         )
     current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = b"admin"
+
+    # Busca o usuário no .env. Se der erro ou não achar o arquivo, usa "" (vazio) por segurança
+    correct_username_bytes = os.getenv("LOGIN_USER", "").encode("utf8")
+
     is_correct_username = secrets.compare_digest(
         current_username_bytes, correct_username_bytes
     )
+
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = b"trazzo2026"
+
+    # Busca a senha no .env. Se der erro ou não achar o arquivo, usa "" (vazio) por segurança
+    correct_password_bytes = os.getenv("LOGIN_PASSWORD", "").encode("utf8")
+
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
+
     if not (is_correct_username and is_correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
